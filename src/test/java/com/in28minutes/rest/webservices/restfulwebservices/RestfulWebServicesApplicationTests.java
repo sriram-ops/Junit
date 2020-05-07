7821349dd9eb8d7c9f09fc00cd499be17dac5c13
Sriram <Bhagavatula.Sriram@CenturyLink.com>
2020-05-08 00:21:28 +0530

    Added annotation in test class

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,10 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.in28minutes.rest.webservices.resdtfulwebservices.user.RestfulWebServicesApplication;
 import com.in28minutes.rest.webservices.resdtfulwebservices.user.UserResource;
@@ -13,8 +15,8 @@ import com.in28minutes.rest.webservices.resdtfulwebservices.user.UserResource;
 
  
  
-
-@SpringBootTest(classes =com.in28minutes.rest.webservices.resdtfulwebservices.user.UserResource.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes =com.in28minutes.rest.webservices.resdtfulwebservices.user.UserResource.class)
 @Import(com.in28minutes.rest.webservices.resdtfulwebservices.user.UserDAOService.class)
 @ContextConfiguration(classes = RestfulWebServicesApplication.class)
 class RestfulWebServicesApplicationTests {
